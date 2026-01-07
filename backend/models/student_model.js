2ac02cf3bc401292fb49e144019de10c53404914
Etunimesi Sukunimesi <shkpostisi@example.com>
2026-01-07 19:42:22 +0200

    CRUD-operaatiot, ei pelaa täydellisesti

--- backend/models/student_model.js
+++ backend/models/student_model.js
@@ -1,27 +1,38 @@
-const { useCallback } = require('react');
 const db = require('../database');
 const bcrypt = require('bcryptjs');
 
 const student = {
-    getAll:function(callback){
-        return db.query("SELECT * FROM students", callback);
+
+    getAll: function (callback) {
+        return db.query("SELECT * FROM student", callback);
     },
-    getOne:function(username,callback){
-        return db.query("SELECT * FROM students WHERE username=?",[username],callback);
+
+    getOne: function (username, callback) {
+        return db.query(
+            "SELECT * FROM student WHERE username = ?",
+            [username],
+            callback
+        );
     },
-    add:function(student,callback){
-        bcrypt.hash(student.password,10,function(err, hashedPassword){
-            return db.query("INSERT INTO student VALUES(?,?,?,?,?,?)",[student.username, student.fname, student.lname, student.streetaddress, student.email, student.hashedPassword],callback)
+
+    add: function (student, callback) {
+        bcrypt.hash(student.password, 10, function (err, hashedPassword) {
+            if (err) return callback(err);
+            return db.query("INSERT INTO student (username, fname, lname, streetaddress, email, password) VALUES (?,?,?,?,?,?)",[student.username,student.fname,student.lname,student.streetaddress,student.email,hashedPassword],callback);
         });
     },
-    update:function(student,username,callback){
-        bcrypt.hash(student.password,10,function(err, hashedPassword){
-            return db.query("UPDATE student SET fname=?,lname=?,streetaddress=?,email=?,password=? WHERE username=?",[student.fname, student.lname, student.streetaddress, student.email, hashedPassword, username],callback)
+
+    update: function (student, username, callback) {
+        bcrypt.hash(student.password, 10, function (err, hashedPassword) {
+            if (err) return callback(err);
+            return db.query("UPDATE student SET fname=?, lname=?, streetaddress=?, email=?, password=? WHERE username=?",[student.fname,student.lname,student.streetaddress,student.email,hashedPassword,username],callback);
         });
     },
-    delete:function(username,callback){
-        return db.query("DELETE FROM students WHERE username=?",[username],callback);
+
+    delete: function (username, callback) {
+        return db.query(
+            "DELETE FROM student WHERE username = ?",[username],callback);
     }
-}
+};
 
 module.exports = student;
